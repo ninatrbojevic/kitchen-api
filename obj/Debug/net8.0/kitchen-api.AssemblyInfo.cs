153457d59695a3c3b08aee0e830ab3d86441b0f0
ninatrbojevic <nina.trbojevic@student.uniri.hr>
2024-08-27 14:06:15 +0200

    Image variable name

--- obj/Debug/net8.0/kitchen-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/kitchen-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kitchen-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc0ce3b62884e2c92ae7a5895941d84375344d19")]
 [assembly: System.Reflection.AssemblyProductAttribute("kitchen-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kitchen-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
